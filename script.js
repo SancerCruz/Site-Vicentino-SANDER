71e906569d161f63602626f13d1a79db8d489b13
SancerCruz <sander3mar@yahoo.com.br>
2024-11-27 09:59:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+
+/* botão leia mais- leia menos*/
+
+function leiaMais(){
+    var pontos=document.getElementById("pontos");
+    var maisTexto=document.getElementById("mais");
+    var btnLeiaMais=document.getElementById("btnLeiaMais");
+
+    if(pontos.style.display === "none"){
+    pontos.style.display="inline";
+    maisTexto.style.display="none";
+    btnLeiaMais.innerHTML="Leia Mais";
+    }
+    else{
+        pontos.style.display="none";
+        maisTexto.style.display="inline";
+        btnLeiaMais.innerHTML="Leia Menos";
+
+
+        /* tenina - botão leia mais- leia menos*/
+    }
+}
+    
+                                /*PADROEIROS*/
+    function leiaMaisss(){
+    var pontos2=document.getElementById("pontos2");
+    var maisssTexto=document.getElementById("maisss");
+    var btnLeiaMais1=document.getElementById("btnLeiaMais1");
+
+    if(pontos2.style.display === "none"){
+    pontos2.style.display="inline";
+    maisssTexto.style.display="none";
+    btnLeiaMais1.innerHTML="Leia Maisss";
+    }
+    else{
+        pontos2.style.display="none";
+        maisssTexto.style.display="inline";
+        btnLeiaMais1.innerHTML="Leia Menosss";
+}
+
+}
+
+/*botão Livro Temático 2024: Assistidos e vicentinos contam sua história*/
+
+
+function leiaMaiss(){
+    var pontos1=document.getElementById("pontos1");
+    var maissTexto=document.getElementById("maiss");
+    var btnLeiaMaiss=document.getElementById("btnLeiaMaiss");
+
+    if(pontos1.style.display === "none"){
+        pontos1.style.display="inline";
+        maissTexto.style.display="none";
+        btnLeiaMaiss.innerHTML="Lendo maiss ";
+    }
+    else{
+        pontos1.style.display="none";
+        maissTexto.style.display="inline";
+        btnLeiaMaiss.innerHTML="Lendo nemos";
+
+
+        /* tenina - botão leia mais- leia menos*/
+    }
+
+
+}
